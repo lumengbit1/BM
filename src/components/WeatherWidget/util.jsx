27caa395e864eaab2534e598555c3bd087b534a8
Meng Lu <meng@burnsred.com.au>
2021-06-16 20:18:50 +1000

    update code

--- src/components/WeatherWidget/util.jsx
+++ src/components/WeatherWidget/util.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import ImmutablePropTypes from 'react-immutable-proptypes';
+import { Map } from 'immutable';
 
 const withComponentLoading = (WrappedComponent) => {
   function WithWrap(props) {
-    const { loading } = props;
+    const { loading, weatherRecords } = props;
 
     if (loading) return 'Loading...';
+
+    if (!weatherRecords || weatherRecords.isEmpty()) return null;
 
     return (
       <WrappedComponent {...props} />
@@ -14,10 +18,12 @@ const withComponentLoading = (WrappedComponent) => {
 
   WithWrap.propTypes = {
     loading: PropTypes.bool,
+    weatherRecords: ImmutablePropTypes.map,
   };
 
   WithWrap.defaultProps = {
     loading: undefined,
+    weatherRecords: Map(),
   };
 
   return WithWrap;
